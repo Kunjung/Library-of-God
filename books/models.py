1e4b73e8d25d31989f443c88475936cc2c75433b
Kunjung <kunjungsherpa@gmail.com>
2019-04-08 14:42:59 +0545

    First Migration - Created Person and Book class

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=64)
+    summary = models.CharField(max_length=300)
+    current_owner = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="ownedbooks")
+
+    def __str__(self):
+        return f"{self.name}"
