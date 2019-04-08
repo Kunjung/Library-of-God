cdfa14e64e091f2952af7a92e74b40909b8eb2ee
Kunjung <kunjungsherpa@gmail.com>
2019-04-08 16:56:00 +0545

    OK now it's started. Ended up choosing a many to many relationship between person and book. The app right now shows a way to list all books at index and book. The app has a way to show all the books available at the index page and a way to see more details of each book at /book

--- books/models.py
+++ books/models.py
@@ -3,10 +3,20 @@ from django.db import models
 # Create your models here.
 
 
+## Trying the Many to Many relationship
+class Book(models.Model):
+    name = models.CharField(max_length=64)
+    summary = models.TextField(max_length=300, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Person(models.Model):
     username = models.CharField(max_length=32, null=True)
     first = models.CharField(max_length=15, null=True)
     last = models.CharField(max_length=15, null=True)
+    owned_books = models.ManyToManyField(Book, blank=True, related_name='owners')
 
     # phone = models.CharField(max_length=11)
     # email = models.CharField(max_length=20)
@@ -15,10 +25,27 @@ class Person(models.Model):
         return f"{self.first} {self.last}"
 
 
-class Book(models.Model):
-    name = models.CharField(max_length=64)
-    summary = models.TextField(max_length=300, null=True)
-    current_owner = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="ownedbooks")
+## Old DataBase Models for the One to many relationship.
+## I can query the database fine. But querying like this:
+## Books.objects.filter(current_owner=p) is gonna be a lot a lot of waste of db
+## also i can't easily see what books a single person owns easily in the admin page
 
-    def __str__(self):
-        return f"{self.name}"
+# class Person(models.Model):
+#     username = models.CharField(max_length=32, null=True)
+#     first = models.CharField(max_length=15, null=True)
+#     last = models.CharField(max_length=15, null=True)
+#
+#     # phone = models.CharField(max_length=11)
+#     # email = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         return f"{self.first} {self.last}"
+#
+#
+# class Book(models.Model):
+#     name = models.CharField(max_length=64)
+#     summary = models.TextField(max_length=300, null=True)
+#     current_owner = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="ownedbooks")
+#
+#     def __str__(self):
+#         return f"{self.name}"
--- books/views.py
+++ books/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+
+from .models import Person, Book
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello")
+    context = {
+        "books": Book.objects.all()
+    }
+    return render(request, "books/index.html", context)
+
+
+def book(request, book_id):
+    try:
+        book = Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist.")
+
+    context = {
+        "book": book,
+        "owners": book.owners.all()
+    }
+    return render(request, "books/book.html", context)
