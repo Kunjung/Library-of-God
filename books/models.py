54eec0a1b445c578ed578171e267459323a42e34
Kunjung <kunjungsherpa@gmail.com>
2019-04-08 15:27:03 +0545

    Stupid Mistake. Gotta do 'makemigrations app_name' to make the table

--- books/models.py
+++ books/models.py
@@ -4,15 +4,20 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=64)
+    username = models.CharField(max_length=32, null=True)
+    first = models.CharField(max_length=15, null=True)
+    last = models.CharField(max_length=15, null=True)
+
+    # phone = models.CharField(max_length=11)
+    # email = models.CharField(max_length=20)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.first} {self.last}"
 
 
 class Book(models.Model):
     name = models.CharField(max_length=64)
-    summary = models.CharField(max_length=300)
+    summary = models.TextField(max_length=300, null=True)
     current_owner = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="ownedbooks")
 
     def __str__(self):
