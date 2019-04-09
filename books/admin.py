e844d37772adb9642c0037e83440499c41727a57
Kunjung <kunjungsherpa@gmail.com>
2019-04-09 08:30:18 +0545

    Added A Better Admin Interface for looking up a person's owned_books. (one where it's separated from all available books).

--- books/admin.py
+++ books/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Book, Person
 
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    filter_horizontal = ("owned_books", )
+
 admin.site.register(Book)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
