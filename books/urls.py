ef6d0dcaab89510b881e1bc924f3d7ef057af986
Kunjung <kunjungsherpa@gmail.com>
2019-04-11 16:34:43 +0545

    2 New Functions: 1. /yourbooks to list all yourbooks and has a form to add new ones. 2. /addbook to add new Books and redirect user back to /yourbooks

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('book/<int:book_id>', views.book, name='book'),
     path('person/<int:person_id>', views.person, name='person'),
     path('person/<int:person_id>/yourbooks', views.yourbooks, name='yourbooks'),
+    path('person/<int:person_id>/addbook', views.addbook, name='addbook'),
     path('wishlist/<int:person_id>', views.wishlist, name='wishlist'),
     path('angel/<int:person_id>', views.angel, name='angel'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,75 +1,94 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 
 
 from .models import Person, Book
 
 # Create your views here.
 def index(request):
-    context = {
-        "books": Book.objects.all()
-    }
-    return render(request, "books/index.html", context)
+	context = {
+		"books": Book.objects.all()
+	}
+	return render(request, "books/index.html", context)
 
 
 def book(request, book_id):
-    try:
-        book = Book.objects.get(pk=book_id)
-    except Book.DoesNotExist:
-        raise Http404("Book does not exist.")
+	try:
+		book = Book.objects.get(pk=book_id)
+	except Book.DoesNotExist:
+		raise Http404("Book does not exist.")
 
-    context = {
-        "book": book
-    }
-    return render(request, "books/book.html", context)
+	context = {
+		"book": book
+	}
+	return render(request, "books/book.html", context)
 
 
 def person(request, person_id):
-    try:
-        person = Person.objects.get(pk=person_id)
-    except Person.DoesNotExist:
-        raise Http404("Person does not exist.")
+	try:
+		person = Person.objects.get(pk=person_id)
+	except Person.DoesNotExist:
+		raise Http404("Person does not exist.")
 
-    context = {
-        "person": person,
-        "books": person.books.all()
-    }
-    return render(request, "books/person.html", context)
+	context = {
+		"person": person,
+		"books": person.books.all()
+	}
+	return render(request, "books/person.html", context)
 
 def yourbooks(request, person_id):
-    try:
-        person = Person.objects.get(pk=person_id)
-    except Person.DoesNotExist:
-        raise Http404("Person does not exist.")
+	try:
+		person = Person.objects.get(pk=person_id)
+	except Person.DoesNotExist:
+		raise Http404("Person does not exist.")
 
-    context = {
-        "person": person,
-        "books": person.books.all()
-    }
-    return render(request, "books/yourbooks.html", context)
+	context = {
+		"person": person,
+		"books": person.books.all()
+	}
+	return render(request, "books/yourbooks.html", context)
+
+def addbook(request, person_id):
+	try:
+		name = request.POST["name"]
+		summary = request.POST["summary"]
+
+		person = Person.objects.get(pk=person_id)
+	except KeyError:
+		return render(request, "books/error.html", {"message": "No selection."})
+	except Person.DoesNotExist:
+		return render(request, "books/error.html", {"message": "No person."})
+	except:
+		return render(request, "books/error.html", {"message": "Unknown Error"})
+
+	new_book = Book(name = name, summary = summary, owner = person)
+	new_book.save()
+
+	return HttpResponseRedirect(reverse("yourbooks", args=(person_id,) ))
 
 
 def wishlist(request, person_id):
-    try:
-        person = Person.objects.get(pk=person_id)
-    except Person.DoesNotExist:
-        raise Http404("Person does not exist.")
+	try:
+		person = Person.objects.get(pk=person_id)
+	except Person.DoesNotExist:
+		raise Http404("Person does not exist.")
 
-    context = {
-        "person": person,
-        "wishes": person.wishes.all()
-    }
-    return render(request, "books/wishlist.html", context)
+	context = {
+		"person": person,
+		"wishes": person.wishes.all()
+	}
+	return render(request, "books/wishlist.html", context)
 
 
 def angel(request, person_id):
-    try:
-        person = Person.objects.get(pk=person_id)
-    except Person.DoesNotExist:
-        raise Http404("Person does not exist.")
+	try:
+		person = Person.objects.get(pk=person_id)
+	except Person.DoesNotExist:
+		raise Http404("Person does not exist.")
 
-    context = {
-        "person": person,
-        "wishes_to_fulfill": person.wishes_to_fulfill.all()
-    }
-    return render(request, "books/angel.html", context)
+	context = {
+		"person": person,
+		"wishes_to_fulfill": person.wishes_to_fulfill.all()
+	}
+	return render(request, "books/angel.html", context)
