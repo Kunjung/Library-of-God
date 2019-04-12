937afa396f9cd92fa3fee615f9e24e711a76b477
Kunjung <kunjungsherpa@gmail.com>
2019-04-12 07:29:01 +0545

    BIG Level Up. /yourwishes and /addwish to add new wishes and see them all in the same place. Making new wishes has a simpler select box that chooses all the books that are not in the person's wish list or that person's owned books list

--- books/urls.py
+++ books/urls.py
@@ -5,9 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('book/<int:book_id>', views.book, name='book'),
+    
     path('person/<int:person_id>', views.person, name='person'),
     path('person/<int:person_id>/yourbooks', views.yourbooks, name='yourbooks'),
     path('person/<int:person_id>/addbook', views.addbook, name='addbook'),
+    path('person/<int:person_id>/yourwishes', views.yourwishes, name='yourwishes'),
+    path('person/<int:person_id>/addwish', views.addwish, name='addwish'),
+
     path('wishlist/<int:person_id>', views.wishlist, name='wishlist'),
     path('angel/<int:person_id>', views.angel, name='angel'),
 ]
--- books/views.py
+++ books/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 
 
-from .models import Person, Book
+from .models import Person, Book, Wish
 
 # Create your views here.
 def index(request):
@@ -76,7 +76,7 @@ def wishlist(request, person_id):
 
 	context = {
 		"person": person,
-		"wishes": person.wishes.all()
+		"wishes": person.wishes.all().order_by('-desire_lvl')
 	}
 	return render(request, "books/wishlist.html", context)
 
@@ -89,6 +89,48 @@ def angel(request, person_id):
 
 	context = {
 		"person": person,
-		"wishes_to_fulfill": person.wishes_to_fulfill.all()
+		"wishes_to_fulfill": person.wishes_to_fulfill.all().order_by('-desire_lvl')
 	}
 	return render(request, "books/angel.html", context)
+
+
+def yourwishes(request, person_id):
+	try:
+		person = Person.objects.get(pk=person_id)
+		book_id_list = [book.id for book in person.books.all()]
+		wish_book_id_list = [wish.book.id for wish in person.wishes.all()]
+
+	except Person.DoesNotExist:
+		raise Http404("Person does not exist.")
+
+	context = {
+		"person": person,
+		"wishes": person.wishes.all(),
+		"newbooks": Book.objects.exclude(id__in = book_id_list).exclude(id__in = wish_book_id_list).all()
+	}
+	return render(request, "books/yourwishes.html", context)
+
+def addwish(request, person_id):
+	try:
+		book_id = request.POST["book_id"]
+		desire_lvl = int(request.POST["desire_lvl"])
+
+		book = Book.objects.get(pk=book_id)
+		wisher = Person.objects.get(pk=person_id)
+
+		angel_id = book.owner.id
+		angel = Person.objects.get(pk=angel_id)
+
+	except KeyError:
+		return render(request, "books/error.html", {"message": "No selection."})
+	except Person.DoesNotExist:
+		return render(request, "books/error.html", {"message": "No wish maker or angel to fulfill the wish. God is Dead."})
+	except Book.DoesNotExist:
+		return render(request, "books/error.html", {"message": "No book."})
+	except:
+		return render(request, "books/error.html", {"message": "Unknown Error"})
+
+	new_wish = Wish(book = book, wisher = wisher, desire_lvl = desire_lvl, angel=angel)
+	new_wish.save()
+
+	return HttpResponseRedirect(reverse("yourwishes", args=(person_id,) ))
