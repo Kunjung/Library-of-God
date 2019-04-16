22f9edada44058a13923d527632a2d4d706cd8ab
Kunjung <kunjungsherpa@gmail.com>
2019-04-16 08:02:30 +0545

    The Algorithm at route /match works for 6 people too

--- load_madeup_data.py
+++ load_madeup_data.py
@@ -24,48 +24,51 @@ print("Done Deleting ALL Data")
 
 print("***************************")
 print("Adding People:")
-kunjung = Person(name="Kunjung")
-mingmar = Person(name="Mingmar")
-aagya = Person(name="Aagya")
-kushal = Person(name="Kushal")
+persons = ["Dolma", "Kunjung", "Mingmar", "Aagya", "Kushal", "Galileo"]
 
-kunjung.save()
-mingmar.save()
-aagya.save()
-kushal.save()
-
+persons_list = []
+for person in persons:
+    p = Person(name=person)
+    p.save()
+    persons_list.append(p)
 
 print("Done Loading People Data")
 print("#######################################")
 
 print("Adding Books")
-b1 = Book(name="Rich Dad Poor Dad", owner=mingmar)
-b2 = Book(name="Reality is Broken", owner=kunjung)
-b3 = Book(name="Man's Search for Meaning", owner=aagya)
-b4 = Book(name="Subtle Art of not giving a F**k", owner=kushal)
-
-b1.save()
-b2.save()
-b3.save()
-b4.save()
+books = ["Win Friends and Influence People",
+        "Power of Habit",
+        "Rich Dad Poor Dad",
+        "Quiet",
+        "Arthatantra",
+        "All of me"
+]
+books_list = []
+for i, book in enumerate(books):
+    b = Book(name=book, owner=persons_list[i]) ## Set the ith book owner to the ith person
+    b.save()
+    books_list.append(b)
 
 print("Done Loading Book Data")
 print("#######################################")
 
 print("Adding Wishes")
-## Kunjung Ranks Rich Dad Poor Dad
-w1 = Wish(book=b1, wisher=kunjung, angel=b1.owner)
-w1.save()
+wish_orders = [
+    (6, 2, 4),
+    (5, 1, 6),
+    (5, 4, 6),
+    (1, 2, 3),
+    (1, 2, 3),
+    (1, 4, 5)
+]
 
-## Mingmar wishes Reality is Broken
-w2 = Wish(book=b2, wisher=mingmar, angel=b2.owner)
-w2.save()
+for i, wish_order in enumerate(wish_orders):
+    wisher = persons_list[i]
+    ### 3 wishes for everyone
+    for rank, j in enumerate(wish_order):
+        book = books_list[j-1]
+        angel = book.owner
+        wish = Wish(book=book, wisher=wisher, angel=angel, rank=rank+1)
+        wish.save()
 
-## Aagya wishes for Reality is Broken
-w3 = Wish(book=b2, wisher=aagya, angel=b2.owner)
-w3.save()
-
-
-## Kushal wishes for Man's Search for Meaning
-w4 = Wish(book=b3, wisher=kushal, angel=b3.owner)
-w4.save()
+print("Done Adding Top 3 Wishes for everyone")
