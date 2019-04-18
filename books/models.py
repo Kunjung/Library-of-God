00ad1a2951cf7ab67d082a8b44fd781c815aba17
Kunjung <kunjungsherpa@gmail.com>
2019-04-18 15:26:50 +0545

    Added Photo field to the Person model class. Now we can see beautiful photos of everyone. Also changed the footer to look better

--- books/models.py
+++ books/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=20, default="Mr Nobody")
+    photo = models.FileField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
