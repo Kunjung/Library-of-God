a02889a1eeceab459139b96224f5c0193c91ff86
lahsuk <kushal.dot.ingnam@gmail.com>
2019-04-20 11:12:54 +0545

    Added nlp for ordering books

---
+++ books/order_nlp.py
@@ -0,0 +1,69 @@
+import math
+
+import numpy as np
+
+import spacy
+
+
+nlp = spacy.load('en_core_web_md')
+# weights for the wished books
+# the book wanted the most takes the most weight
+# and the book wanted on the last of the list
+# takes least weight
+weights = np.array([
+    2, 1.8, 1.6, 1.4, 1.2, 1.1, 1
+])
+
+def cosine_similarity(vec1, vec2):
+    similarity = vec1.dot(vec2) / ((vec1 ** 2).sum() * (vec2 ** 2).sum())
+    return similarity
+
+def order_remaining_wishes(wished_books_list, all_books_list):
+    """
+    params:
+        wished_books_list: an ordered list of books
+        all_books_list: the list of all the books
+    return:
+        list of all book in order of preference
+    """
+    wished_books_vector = np.zeros((300,)) # spacy's word vector has shape (300,)
+    # find the weighted average of all the vectors of the books name
+    for i in range(len(wished_books_list)):
+        wished_book = wished_books_list[i]
+        doc = nlp(wished_book)
+        wished_books_vector += weights[i] * doc.vector
+    wished_books_vector /= np.sum(weights)
+
+    # copy it, since we're going to change it
+    new_all_books_list = all_books_list[:]
+    for wished_book in wished_books_list:
+        new_all_books_list.remove(wished_book)
+
+    similarity_scores = []
+    for i in range(len(new_all_books_list)):
+        book = new_all_books_list[i]
+        doc = nlp(book)
+        similarity = cosine_similarity(wished_books_vector, doc.vector)
+        similarity_scores.append((i, similarity))
+
+    # some words may not be in the vocabulary so it may return NaN when
+    # computing similarity; replace the score with zero
+    for i in range(len(similarity_scores)):
+        index, similarity = similarity_scores[i]
+        if math.isnan(similarity):
+            similarity_scores[i] = (index, 0.0)
+
+    similarity_scores = sorted(similarity_scores,
+                               key=lambda similarity: similarity[1],
+                               reverse=True)
+
+#     for similarity_score in similarity_scores:
+#         i, similarity = similarity_score
+#         print(new_all_books_list[i], similarity)
+
+    scored_books_list = []
+    for similarity_score in similarity_scores:
+        index, similarity = similarity_score
+        scored_books_list.append(new_all_books_list[index])
+
+    return scored_books_list
--- books/views.py
+++ books/views.py
@@ -7,92 +7,93 @@ from .models import Person, Book, Wish, Exchange
 
 ### The Matching Algorithm ###
 from .algorithm_king_and_queen import begin_King_and_Queen_Match
+from .order_nlp import order_remaining_wishes
 
 # Create your views here.
 
 ## Index Page shows all the books that have the Field value of "Available" = True
 def index(request):
-	context = {
-		"books": Book.objects.all(),
-		"book_number": len(Book.objects.all()),
-		"people_number": len(Person.objects.all())
-	}
-	return render(request, "books/index.html", context)
+    context = {
+        "books": Book.objects.all(),
+        "book_number": len(Book.objects.all()),
+        "people_number": len(Person.objects.all())
+    }
+    return render(request, "books/index.html", context)
 
 
 ## Show details of a Particular Book
 def book(request, book_id):
-	try:
-		book = Book.objects.get(pk=book_id)
-	except Book.DoesNotExist:
-		raise Http404("Book does not exist.")
+    try:
+        book = Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist.")
 
-	context = {
-		"book": book
-	}
-	return render(request, "books/book.html", context)
+    context = {
+        "book": book
+    }
+    return render(request, "books/book.html", context)
 
 ## Show details of a Particular Person
 def person(request, person_id):
-	try:
-		person = Person.objects.get(pk=person_id)
-	except Person.DoesNotExist:
-		raise Http404("Person does not exist.")
+    try:
+        person = Person.objects.get(pk=person_id)
+    except Person.DoesNotExist:
+        raise Http404("Person does not exist.")
 
-	context = {
-		"person": person,
-		"books": person.books.all(),
-		"wishes": person.wishes.all(),
-		"matched_wishes": person.wishes.filter(fulfilled=True)
-	}
-	return render(request, "books/person.html", context)
+    context = {
+        "person": person,
+        "books": person.books.all(),
+        "wishes": person.wishes.all(),
+        "matched_wishes": person.wishes.filter(fulfilled=True)
+    }
+    return render(request, "books/person.html", context)
 
 ## Show all the books of a particular Person
 def yourbooks(request, person_id):
-	try:
-		person = Person.objects.get(pk=person_id)
-	except Person.DoesNotExist:
-		raise Http404("Person does not exist.")
+    try:
+        person = Person.objects.get(pk=person_id)
+    except Person.DoesNotExist:
+        raise Http404("Person does not exist.")
 
-	context = {
-		"person": person,
-		"books": person.books.all(),
-	}
-	return render(request, "books/yourbooks.html", context)
+    context = {
+        "person": person,
+        "books": person.books.all(),
+    }
+    return render(request, "books/yourbooks.html", context)
 
 def changebookavailability(request, person_id):
-	try:
-		book_id = int(request.POST["book_id"])
-		book_available = request.POST.get("available", False)
-		book = Book.objects.get(pk=book_id)
-		person = Person.objects.get(pk=person_id)
-		book_available = bool(book_available)
+    try:
+        book_id = int(request.POST["book_id"])
+        book_available = request.POST.get("available", False)
+        book = Book.objects.get(pk=book_id)
+        person = Person.objects.get(pk=person_id)
+        book_available = bool(book_available)
 
-		if book.owner != person:
-			return render(request, "books/error.html", {"message": "Can't change someone else's book Boy or Girl."})
+        if book.owner != person:
+            return render(request, "books/error.html", {"message": "Can't change someone else's book Boy or Girl."})
 
-		### STOP if the book is waiting to be exchanged
-		### Here the condition will be true when The Exchange object for the person has been created and the meeting is False
-		### and also when the wish object is there where the Wish.angel is the person and fulfilled = True
-		wish2fulfill = person.wishes_to_fulfill.filter(book=book).filter(fulfilled=True)
-		if len(wish2fulfill) >= 1:
-			message = "Can't change the availability now. You fulfilled the wish and The Book is waiting to be exchanged to someone else."
-			return render(request, "books/error.html", {"message": message})
+        ### STOP if the book is waiting to be exchanged
+        ### Here the condition will be true when The Exchange object for the person has been created and the meeting is False
+        ### and also when the wish object is there where the Wish.angel is the person and fulfilled = True
+        wish2fulfill = person.wishes_to_fulfill.filter(book=book).filter(fulfilled=True)
+        if len(wish2fulfill) >= 1:
+            message = "Can't change the availability now. You fulfilled the wish and The Book is waiting to be exchanged to someone else."
+            return render(request, "books/error.html", {"message": message})
 
-	except KeyError:
-		# available = request.POST["available"]
-		message = "Key Error " + str(book_available)
-		print(message)
-		return render(request, "books/error.html", {"message": message})
-	except Person.DoesNotExist:
-		return render(request, "books/error.html", {"message": "No person."})
-	except Book.DoesNotExist:
-		return render(request, "books/error.html", {"message": "No book."})
-	except:
-		return render(request, "books/error.html", {"message": "Unknown Error"})
-	book.available = book_available
-	book.save()
-	return HttpResponseRedirect(reverse("yourbooks", args=(person_id,) ))
+    except KeyError:
+        # available = request.POST["available"]
+        message = "Key Error " + str(book_available)
+        print(message)
+        return render(request, "books/error.html", {"message": message})
+    except Person.DoesNotExist:
+        return render(request, "books/error.html", {"message": "No person."})
+    except Book.DoesNotExist:
+        return render(request, "books/error.html", {"message": "No book."})
+    except:
+        return render(request, "books/error.html", {"message": "Unknown Error"})
+    book.available = book_available
+    book.save()
+    return HttpResponseRedirect(reverse("yourbooks", args=(person_id,) ))
 
 # ## Let a person add new books
 # def addbook(request, person_id):
@@ -121,181 +122,185 @@ def changebookavailability(request, person_id):
 
 ## Show all the wishes of a particular person
 def yourwishes(request, person_id):
-	try:
-		person = Person.objects.get(pk=person_id)
-		book_id_list = [book.id for book in person.books.all()]
-		wish_book_id_list = [wish.book.id for wish in person.wishes.all()]
+    try:
+        person = Person.objects.get(pk=person_id)
+        book_id_list = [book.id for book in person.books.all()]
+        wish_book_id_list = [wish.book.id for wish in person.wishes.all()]
 
-		wished_ranks = [wish.rank for wish in person.wishes.all()]
+        wished_ranks = [wish.rank for wish in person.wishes.all()]
 
-	except Person.DoesNotExist:
-		raise Http404("Person does not exist.")
+    except Person.DoesNotExist:
+        raise Http404("Person does not exist.")
 
-	context = {
-		"person": person,
-		"wishes": person.wishes.all(),
-		"available_ranks": [ rank for rank in [1, 2, 3] if rank not in wished_ranks ],
-		"newbooks": Book.objects.exclude(id__in = book_id_list).exclude(id__in = wish_book_id_list).all()
-	}
-	return render(request, "books/yourwishes.html", context)
+    context = {
+        "person": person,
+        "wishes": person.wishes.all(),
+        "available_ranks": [ rank for rank in [1, 2, 3] if rank not in wished_ranks ],
+        "newbooks": Book.objects.exclude(id__in = book_id_list).exclude(id__in = wish_book_id_list).all()
+    }
+    return render(request, "books/yourwishes.html", context)
 
 def addwish(request, person_id):
-	try:
-		book_id = request.POST["book_id"]
-		rank = int(request.POST["rank"])
+    try:
+        book_id = request.POST["book_id"]
+        rank = int(request.POST["rank"])
 
-		book = Book.objects.get(pk=book_id)
-		wisher = Person.objects.get(pk=person_id)
+        book = Book.objects.get(pk=book_id)
+        wisher = Person.objects.get(pk=person_id)
 
-		angel_id = book.owner.id
-		angel = Person.objects.get(pk=angel_id)
+        angel_id = book.owner.id
+        angel = Person.objects.get(pk=angel_id)
 
-	except KeyError:
-		return render(request, "books/error.html", {"message": "No selection."})
-	except Person.DoesNotExist:
-		return render(request, "books/error.html", {"message": "No wish maker or angel to fulfill the wish. God is Dead."})
-	except Book.DoesNotExist:
-		return render(request, "books/error.html", {"message": "No book."})
-	except:
-		return render(request, "books/error.html", {"message": "Unknown Error"})
+    except KeyError:
+        return render(request, "books/error.html", {"message": "No selection."})
+    except Person.DoesNotExist:
+        return render(request, "books/error.html", {"message": "No wish maker or angel to fulfill the wish. God is Dead."})
+    except Book.DoesNotExist:
+        return render(request, "books/error.html", {"message": "No book."})
+    except:
+        return render(request, "books/error.html", {"message": "Unknown Error"})
 
-	new_wish = Wish(book = book, wisher = wisher, rank = rank, angel=angel)
-	new_wish.save()
+    new_wish = Wish(book = book, wisher = wisher, rank = rank, angel=angel)
+    new_wish.save()
 
-	return HttpResponseRedirect(reverse("yourwishes", args=(person_id,) ))
+    return HttpResponseRedirect(reverse("yourwishes", args=(person_id,) ))
 
 
 def yourmatches(request, person_id):
-	try:
-		person = Person.objects.get(pk=person_id)
-		mybook = person.books.all().first()
+    try:
+        person = Person.objects.get(pk=person_id)
+        mybook = person.books.all().first()
 
-	except Person.DoesNotExist:
-		raise Http404("Person does not exist.")
-	except Book.DoesNotExist:
-		raise Http404("Book does not exist.")
+    except Person.DoesNotExist:
+        raise Http404("Person does not exist.")
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist.")
 
-	context = {
-		"person": person,
-		"mybook": mybook,
-		"matched_wishes": person.wishes.filter(fulfilled=True)
-	}
-	return render(request, "books/yourmatches.html", context)
+    context = {
+        "person": person,
+        "mybook": mybook,
+        "matched_wishes": person.wishes.filter(fulfilled=True)
+    }
+    return render(request, "books/yourmatches.html", context)
 
 
 
 def allexchanges(request):
-	exchanges = Exchange.objects.all()
+    exchanges = Exchange.objects.all()
 
-	context = {
-		"exchanges": exchanges,
-	}
-	return render(request, "books/allexchanges.html", context)
+    context = {
+        "exchanges": exchanges,
+    }
+    return render(request, "books/allexchanges.html", context)
 
 
 
 def yourexchanges(request, person_id):
-	try:
-		person = Person.objects.get(pk=person_id)
-		exchanges = Exchange.objects.filter(king=person).exclude(meeting=True)
-	except Person.DoesNotExist:
-		raise Http404("Person does not exist.")
-	except Exchange.DoesNotExist:
-		raise Http404("Exchange does not exist.")
+    try:
+        person = Person.objects.get(pk=person_id)
+        exchanges = Exchange.objects.filter(king=person).exclude(meeting=True)
+    except Person.DoesNotExist:
+        raise Http404("Person does not exist.")
+    except Exchange.DoesNotExist:
+        raise Http404("Exchange does not exist.")
 
-	context = {
-		"person": person,
-		"exchanges": exchanges,
-	}
-	return render(request, "books/yourexchanges.html", context)
+    context = {
+        "person": person,
+        "exchanges": exchanges,
+    }
+    return render(request, "books/yourexchanges.html", context)
 
 
 #######################################################################################################################################################
 ################# King and Queen Matching View  ############################################################################################################
 def match(request):
 
-	all_books = Book.objects.filter(available=True)		### All books will be kings and queens at the same time. They'll have the rank order of last for themselves
+    all_books = Book.objects.filter(available=True)		### All books will be kings and queens at the same time. They'll have the rank order of last for themselves
 
-	complete_book_collection = Book.objects.all()
+    complete_book_collection = Book.objects.all()
 
-	## Trigger King Queen Matching Algorithm only when available books is at least 2/3 of all complete book collection
-	## Stop it if it fails the Condition #1.
-	if len(all_books) < 2/3 * len(complete_book_collection):
-		message = "Algorithm not ready. Total Book Number: " + str(len(complete_book_collection)) + " .  Available Book Number: " + str(len(all_books))
-		return render(request, "books/error.html", {"message": message})
+    ## Trigger King Queen Matching Algorithm only when available books is at least 2/3 of all complete book collection
+    ## Stop it if it fails the Condition #1.
+    if len(all_books) < 2/3 * len(complete_book_collection):
+        message = "Algorithm not ready. Total Book Number: " + str(len(complete_book_collection)) + " .  Available Book Number: " + str(len(all_books))
+        return render(request, "books/error.html", {"message": message})
 
-	## STOP CONDITION #2. Stop King Queen Match when not enough wishes are made.
-	## Every 2/3 person should make at least 3 wishes. Otherwise, don't run algorithm
-	all_persons = Person.objects.all()
+    ## STOP CONDITION #2. Stop King Queen Match when not enough wishes are made.
+    ## Every 2/3 person should make at least 3 wishes. Otherwise, don't run algorithm
+    all_persons = Person.objects.all()
 
+    faces = [book.id for book in all_books]			### For now just check on the book names. later find the book id, okay. bro
+    all_king_preferences = []
+    all_books_name = [book.name for book in Book.objects.all()]
 
-	faces = [book.id for book in all_books]			### For now just check on the book names. later find the book id, okay. bro
-	all_king_preferences = []
+    for book in all_books:
+        all_books_name_copy = list(all_books_name)
+        all_books_name_copy.remove(book.name)
+        king_preference = []
+        wishes = book.owner.wishes.filter(fulfilled=False).order_by('rank')
+        queen_books = [wish.book.id for wish in wishes]
+        for queen_book in queen_books:
+            king_preference.append(queen_book)
 
-	for book in all_books:
-		king_preference = []
-		wishes = book.owner.wishes.filter(fulfilled=False).order_by('rank')
-		queen_books = [wish.book.id for wish in wishes]
-		for queen_book in queen_books:
-			king_preference.append(queen_book)
+        ### Make sure the remaining_books is ordered in a random way for every user
+        remaining_books = Book.objects.exclude(id__in=king_preference).exclude(id__in=[book.id])
+        wished_books = [wish.book.name for wish in wishes]
+        remaining_books = order_remaining_wishes(wished_books, all_books_name_copy)
+        for r_book in remaining_books:
+            book = Book.objects.filter(name=r_book).first()
+            king_preference.append(book.id)
 
-		### Make sure the remaining_books is ordered in a random way for every user
-		remaining_books = Book.objects.exclude(id__in=king_preference).exclude(id__in=[book.id])
-		for r_book in remaining_books:
-			king_preference.append(r_book.id)
+        ### Adding yourself to the very end for the non match
+        king_preference.append(book.id)
 
-		### Adding yourself to the very end for the non match
-		king_preference.append(book.id)
+        ### ADDING king_preference of book b[i]
+        all_king_preferences.append(king_preference)
 
-		### ADDING king_preference of book b[i]
-		all_king_preferences.append(king_preference)
-
-	### Now change all the king preferences from being strings to being indexes based on the faces list
-	all_king_preferences_indexed = []
-	for king_preference in all_king_preferences:
-		king_preference_indexed = [faces.index(king_p) for king_p in king_preference]
-		all_king_preferences_indexed.append(king_preference_indexed)
-
-
-	### King and Queen preferences are the same for the same book
-	matches = begin_King_and_Queen_Match(faces, king_preferences=all_king_preferences_indexed, queen_preferences=all_king_preferences_indexed)
-	info_matches = []
-	for (king_id, queen_id) in matches:
-		king_book = Book.objects.get(pk=king_id)
-		queen_book = Book.objects.get(pk=queen_id)
-		king_owner = king_book.owner
-		queen_owner = queen_book.owner
-		info_match = f"{king_book} (Person: {king_owner}) --------> matched to ---------> {queen_book} (Person: {queen_owner}) "
-		info_matches.append(info_match)
+    ### Now change all the king preferences from being strings to being indexes based on the faces list
+    all_king_preferences_indexed = []
+    for king_preference in all_king_preferences:
+        king_preference_indexed = [faces.index(king_p) for king_p in king_preference]
+        all_king_preferences_indexed.append(king_preference_indexed)
 
 
-	### Set all the kings wishes to fulfilled = True
-	for (king_id, queen_id) in matches:
-		king_book = Book.objects.get(pk=king_id)
-		queen_book = Book.objects.get(pk=queen_id)
-		king = king_book.owner
-		queen = queen_book.owner
-		
-		### The queen's book is what is being wished for, the king makes the wish so he is the wisher and the queen is the angel
-		king_wish = Wish.objects.filter(book=queen_book, wisher=king, angel=queen).first()
-		if king_wish:
-			king_wish.fulfilled = True
-			king_wish.save()
-
-			### Now the queen's book becomes not available for anyone else. It is married now.
-			queen_book.available = False
-			queen_book.save()
-
-		### For each king and queen pair, now make the exchange objects and set the meeting value to false
-		exchange = Exchange(king=king, queen=queen, meeting=False, kingbook=king_book, queenbook=queen_book) ### Not met yet. Gonna meet soon.
-		exchange.save()
+    ### King and Queen preferences are the same for the same book
+    matches = begin_King_and_Queen_Match(faces, king_preferences=all_king_preferences_indexed, queen_preferences=all_king_preferences_indexed)
+    info_matches = []
+    for (king_id, queen_id) in matches:
+        king_book = Book.objects.get(pk=king_id)
+        queen_book = Book.objects.get(pk=queen_id)
+        king_owner = king_book.owner
+        queen_owner = queen_book.owner
+        info_match = f"{king_book} (Person: {king_owner}) --------> matched to ---------> {queen_book} (Person: {queen_owner}) "
+        info_matches.append(info_match)
 
 
-	context = {
-		"matches": info_matches
-	}
+    ### Set all the kings wishes to fulfilled = True
+    for (king_id, queen_id) in matches:
+        king_book = Book.objects.get(pk=king_id)
+        queen_book = Book.objects.get(pk=queen_id)
+        king = king_book.owner
+        queen = queen_book.owner
 
-	return render(request, "books/match.html", context)
+        ### The queen's book is what is being wished for, the king makes the wish so he is the wisher and the queen is the angel
+        king_wish = Wish.objects.filter(book=queen_book, wisher=king, angel=queen).first()
+        if king_wish:
+            king_wish.fulfilled = True
+            king_wish.save()
+
+            ### Now the queen's book becomes not available for anyone else. It is married now.
+            queen_book.available = False
+            queen_book.save()
+
+        ### For each king and queen pair, now make the exchange objects and set the meeting value to false
+        exchange = Exchange(king=king, queen=queen, meeting=False, kingbook=king_book, queenbook=queen_book) ### Not met yet. Gonna meet soon.
+        exchange.save()
+
+    context = {
+        "matches": info_matches
+    }
+
+    return render(request, "books/match.html", context)
 
 
 ###############################################################################################################################################
